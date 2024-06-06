a000ea3e0ac4957ea5371ff189ce7c8b411ed376
Camdin-Dirn <97698102+Camdin-Dirn@users.noreply.github.com>
2024-06-05 21:30:06 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,6 +1,9 @@
-#!/usr/bin/python3
+##!/usr/bin/python3
 # -*-coding:utf-8 -*
 import sys 
-
-[[print(\'%s\t%s\' % (word, 1)) for word in [\'\'.join([c for c in chars if c.isalpha()]) for chars in line.split()] if word] for line in sys.stdin]
-
+  
+for line in sys.stdin: 
+    line = line.strip() 
+    words = line.split() 
+    for word in words:
+        print('%s\t%s' % (word, 1))
