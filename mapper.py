044b28c009587d56481829ae26ea9fbad7c750ad
Camdin-Dirn <97698102+Camdin-Dirn@users.noreply.github.com>
2024-06-12 12:48:36 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,9 +1,12 @@
 ##!/usr/bin/python3
 # -*-coding:utf-8 -*
-import sys 
-  
-for line in sys.stdin: 
-    line = line.strip() 
-    words = line.split() 
+import sys
+import re
+
+word_pattern = re.compile(r'[a-zA-Z]+')
+
+for line in sys.stdin:
+    line = line.strip()
+    words = word_pattern.findall(line)
     for word in words:
-        print('%s\t%s' % (word, 1))
+        print(f'{word}\t1')
